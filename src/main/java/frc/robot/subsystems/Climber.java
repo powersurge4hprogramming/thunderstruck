c2bdc0858ff322974d88e315b1e0fab18cc99df8
Michael Torres <mrmojorisen0@gmail.com>
2026-01-24 00:04:14 -0500

    Get the climbing api going.

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -1,7 +1,23 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Climber extends SubsystemBase {
 
+    /**
+     * {@summary}
+     * Tell the climber to go upward.
+     */
+    public Command upward() {
+        throw new RuntimeException("Not implemented yet.");
+    }
+
+    /**
+     * {@summary}
+     * Tell the climber to go downward.
+     */
+    public Command downward() {
+        throw new RuntimeException("Not implemented yet.");
+    }
 }
