8286dfb7891bf5d1edf1fc8e51705f67c9beef6b
Michael Torres <mrmojorisen0@gmail.com>
2026-01-23 22:30:45 -0500

    Got an AimCamera class.

--- src/main/java/frc/robot/vision/AimCamera.java
+++ src/main/java/frc/robot/vision/AimCamera.java
@@ -1,5 +1,21 @@
 package frc.robot.vision;
 
-public class AimCamera {
+import edu.wpi.first.math.geometry.Transform3d;
 
+public class AimCamera {
+    /**
+     * {@summary}
+     * This gets the {@link Transform3d} when the camera is pointed at the Hub
+     * AprilTag(s).
+     * 
+     * @apiNote
+     *          This should be called when the camera is already
+     *          pointing at the Hub.
+     * 
+     * @return The {@link Transform3d} when the camera is pointed at the Hub, or
+     *         null if it isn't.
+     */
+    public Transform3d getHubRelativeLocation() throws RuntimeException {
+        throw new RuntimeException("Not implemented yet.");
+    }
 }
