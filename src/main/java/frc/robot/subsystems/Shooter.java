ad7fd36c86345e1caaa6ae76d5ae51b7dd171643
Michael Torres <mrmojorisen0@gmail.com>
2026-01-23 23:52:16 -0500

    Adding some motors for the Shooter. I set the follower motor--the second driving motor--to be tied to the main motor.

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,9 +1,24 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.MotorAlignmentValue;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.CANBus;
 
 public class Shooter extends SubsystemBase {
+    private final TalonFX motorLeader;
+    private final TalonFX motorFollower;
+
+    public Shooter() {
+        this.motorLeader = new TalonFX(CANBus.ID.SHOOTER.LEADER, CANBus.BUS.CANIVORE);
+        this.motorFollower = new TalonFX(CANBus.ID.SHOOTER.FOLLOWER, CANBus.BUS.CANIVORE);
+
+        this.motorFollower.setControl(new Follower(this.motorLeader.getDeviceID(), MotorAlignmentValue.Aligned));
+    }
+
     /**
      * This function creates a single command to shoot the ball at a particular
      * instant. It will shoot the ball at any
