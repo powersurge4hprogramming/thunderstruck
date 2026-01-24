d50bd57e096921d821d13c2728fd148a8aedbdac
Michael Torres <mrmojorisen0@gmail.com>
2026-01-24 00:19:18 -0500

    Formatting.

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -6,6 +6,7 @@ import com.ctre.phoenix6.signals.MotorAlignmentValue;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 import frc.robot.CANBus;
 
 public class Shooter extends SubsystemBase {
