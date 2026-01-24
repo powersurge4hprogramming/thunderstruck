f4d519a3ba23c4407b9aea36c1154f8e7e17acad
Michael Torres <mrmojorisen0@gmail.com>
2026-01-23 21:41:02 -0500

    Oh boy... it is time! The shooter subsystem and the climber subsystem has been created. There is still alot of work to do, but this gives an api for the students.
    P.S.
    The WPILib version has been updated. Windows 11 computer has been updated.

---
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -0,0 +1,17 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Shooter extends SubsystemBase {
+    /**
+     * This function creates a single command to shoot the ball at a particular
+     * instant. It will shoot the ball at any
+     * given velocity and launch angle
+     * 
+     * @param velocityBall
+     */
+    public Command shootBall(final double velocityBall, final double angleLaunch) throws RuntimeException {
+        throw new RuntimeException("Did implement the shootBall method yet");
+    }
+}
