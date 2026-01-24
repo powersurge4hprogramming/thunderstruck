62060a057d54796286ba8353e90b53b8ef109cf5
Michael Torres <mrmojorisen0@gmail.com>
2026-01-23 23:56:51 -0500

    Some JavaDocs.

--- src/main/java/frc/robot/CANBus.java
+++ src/main/java/frc/robot/CANBus.java
@@ -1,9 +1,17 @@
 package frc.robot;
 
+/**
+ * {@summary}
+ * This holds all of the static constant values on the CANBus that we are using.
+ */
 public class CANBus {
     // =================================================================================================================
     // Buses
     // =================================================================================================================
+    /**
+     * {@summary}
+     * This holds all of the CANBus names.
+     */
     public static final class BUS {
         public static final String RIO = com.ctre.phoenix6.CANBus.roboRIO().getName();
         public static final String CANIVORE = "canivore";
@@ -12,7 +20,15 @@ public class CANBus {
     // =================================================================================================================
     // IDs
     // =================================================================================================================
+    /**
+     * {@summary}
+     * This holds all of the CANBus IDs.
+     */
     public static final class ID {
+        /**
+         * {@summary}
+         * The IDs for the {@link frc.robot.subsystems.Shooter Shooter}.
+         */
         public static final class SHOOTER {
             public static final short LEADER = 0;
             public static final short FOLLOWER = 0;
