b663645bf9726990092c4fc5d71d6e1460c402c4
Michael Torres <mrmojorisen0@gmail.com>
2026-01-23 23:21:58 -0500

    Made a new Collector class to operate the collector.

---
+++ src/main/java/frc/robot/subsystems/Collector.java
@@ -0,0 +1,23 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Collector extends SubsystemBase {
+
+    /**
+     * {@summary}
+     * Run the collector.
+     */
+    public Command collect() {
+        throw new RuntimeException("Not implemented yet.");
+    }
+
+    /**
+     * {@summary}
+     * Stop the collector.
+     */
+    public Command collectStop() {
+        throw new RuntimeException("Not implemented yet.");
+    }
+}
