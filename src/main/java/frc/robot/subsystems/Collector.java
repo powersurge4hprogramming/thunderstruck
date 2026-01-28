6e6a7ab384e10c09fe088c1aa76325673f88ebf6
mikeyT42 <mrmojorisen0@gmail.com>
2026-01-27 23:26:02 -0500

    I changed the doubles to DoubleSuppliers.

--- src/main/java/frc/robot/subsystems/Collector.java
+++ src/main/java/frc/robot/subsystems/Collector.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems;
 
+import java.util.function.DoubleSupplier;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -8,10 +10,12 @@ public class Collector extends SubsystemBase {
     /**
      * {@summary}
      * Run the collector.
+     *
+     * @param motorRpmScalar A value from 0 to 1 that represents the percentage at which to run the motor.
      * 
      * @return The {@link Command} to perform the action.
      */
-    public Command run() {
+    public Command run(final DoubleSupplier motorRpmScalar) {
         throw new RuntimeException("Not implemented yet.");
     }
 
