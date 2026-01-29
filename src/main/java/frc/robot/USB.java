b7a67458ae36b96a2d574f7e4d94b294e1968b98
Michael Torres <mrmojorisen0@gmail.com>
2026-01-29 14:31:19 -0500

    Adding a comment.

--- src/main/java/frc/robot/USB.java
+++ src/main/java/frc/robot/USB.java
@@ -5,6 +5,10 @@ package frc.robot;
  * This holds all of the static constant values on the USB that we are using.
  */
 public class USB {
+    /**
+     * {@summary}
+     * This holds all of the controller ports we are using.
+     */
     public static final class CONTROLLER {
         public static final short DRIVER = 0;
     }
