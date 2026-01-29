56f99f023d26b33bcdb70efd098600762d262453
Michael Torres <mrmojorisen0@gmail.com>
2026-01-29 11:51:04 -0500

    I think I have the AimCamera::getHubRelativeLocation done. Just needs to be tested.

--- src/main/java/frc/robot/vision/AimCamera.java
+++ src/main/java/frc/robot/vision/AimCamera.java
@@ -1,8 +1,40 @@
 package frc.robot.vision;
 
+import static edu.wpi.first.units.Units.Degrees;
+import static edu.wpi.first.units.Units.Inches;
+
+import org.photonvision.PhotonCamera;
+import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.targeting.PhotonTrackedTarget;
+
+import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.Distance;
 
 public class AimCamera {
+    // =================================================================================================================
+    // Constants
+    // =================================================================================================================
+    private final static short HUB_CENTER = 9;
+    private final static short HUB_OFF_CENTER_RIGHT = 10;
+    private final static Transform3d SHOOTER_TO_CAMERA_OFFSET = new Transform3d(
+            Distance.ofBaseUnits(5, Inches),
+            Distance.ofBaseUnits(0, Inches),
+            Distance.ofBaseUnits(-12, Inches),
+            new Rotation3d(
+                    Angle.ofBaseUnits(0, Degrees),
+                    Angle.ofBaseUnits(0, Degrees),
+                    Angle.ofBaseUnits(0, Degrees)));
+
+    // =================================================================================================================
+    // Systems
+    // =================================================================================================================
+    private final PhotonCamera camera;
+
+    public AimCamera() {
+        this.camera = new PhotonCamera("photonvision");
+    }
 
     /**
      * {@summary}
@@ -17,6 +49,18 @@ public class AimCamera {
      *         null if it isn't.
      */
     public Transform3d getHubRelativeLocation() {
-        throw new RuntimeException("Not implemented yet.");
+        Transform3d hub = null;
+
+        for (final PhotonPipelineResult result : camera.getAllUnreadResults()) {
+            for (final PhotonTrackedTarget target : result.getTargets()) {
+                if (target.fiducialId != HUB_CENTER || target.fiducialId != HUB_OFF_CENTER_RIGHT)
+                    continue;
+                hub = target.getBestCameraToTarget();
+            }
+        }
+        if (hub != null)
+            hub.plus(SHOOTER_TO_CAMERA_OFFSET);
+
+        return hub;
     }
 }
