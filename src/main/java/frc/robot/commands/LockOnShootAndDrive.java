599152c43ef133c2f7cb2f33001f3b6280317ced
Michael Torres <mrmojorisen0@gmail.com>
2026-01-29 22:53:44 -0500

    NOt sure why it's formatting like this, whatever. I removed the setting of the launch angle.

--- src/main/java/frc/robot/commands/LockOnShootAndDrive.java
+++ src/main/java/frc/robot/commands/LockOnShootAndDrive.java
@@ -18,108 +18,109 @@ import frc.robot.physics.ballistics.VelocityAngleSolver.ShotResult;
 import frc.robot.physics.rotational.VelocityToRPMSolver;
 
 public class LockOnShootAndDrive extends Command {
-    // =================================================================================================================
-    // Sub-Systems
-    // =================================================================================================================
-    private final Shooter shooter;
-    private final CommandSwerveDrivetrain drive;
+        // =============================================================================================================
+        // Sub-Systems
+        // =============================================================================================================
+        private final Shooter shooter;
+        private final CommandSwerveDrivetrain drive;
 
-    // =================================================================================================================
-    // Systems
-    // =================================================================================================================
-    private final AimCamera aimCamera;
+        // =============================================================================================================
+        // Systems
+        // =============================================================================================================
+        private final AimCamera aimCamera;
 
-    // =================================================================================================================
-    // Data Suppliers
-    // =================================================================================================================
-    // Allow driver to still move X/Y
-    private final DoubleSupplier xSupplier;
-    private final DoubleSupplier ySupplier;
+        // =============================================================================================================
+        // Data Suppliers
+        // =============================================================================================================
+        // Allow driver to still move X/Y
+        private final DoubleSupplier xSupplier;
+        private final DoubleSupplier ySupplier;
 
-    // =================================================================================================================
-    // Solvers
-    // =================================================================================================================
-    private final VelocityAngleSolver vaSolver;
-    private final VelocityToRPMSolver vRpmSolver;
+        // =============================================================================================================
+        // Solvers
+        // =============================================================================================================
+        private final VelocityAngleSolver vaSolver;
+        private final VelocityToRPMSolver vRpmSolver;
 
-    // =================================================================================================================
-    // Swerve Drive Configurations
-    // =================================================================================================================
-    final SwerveRequest.FieldCentricFacingAngle fieldFacingAngle;
+        // =============================================================================================================
+        // Swerve Drive Configurations
+        // =============================================================================================================
+        final SwerveRequest.FieldCentricFacingAngle fieldFacingAngle;
 
-    // =================================================================================================================
-    // Public Methods
-    // =================================================================================================================
-    public LockOnShootAndDrive(final Shooter shooter, final CommandSwerveDrivetrain drive, final AimCamera aimCamera,
-            final DoubleSupplier xMove, final DoubleSupplier yMove, final DoubleSupplier batteryVoltageSupplier,
-            final double MaxSpeed) {
-        this.shooter = shooter;
-        this.drive = drive;
-        this.aimCamera = aimCamera;
-        this.xSupplier = xMove;
-        this.ySupplier = yMove;
+        // =============================================================================================================
+        // Public Methods
+        // =============================================================================================================
+        public LockOnShootAndDrive(final Shooter shooter, final CommandSwerveDrivetrain drive,
+                        final AimCamera aimCamera,
+                        final DoubleSupplier xMove, final DoubleSupplier yMove,
+                        final DoubleSupplier batteryVoltageSupplier,
+                        final double MaxSpeed) {
+                this.shooter = shooter;
+                this.drive = drive;
+                this.aimCamera = aimCamera;
+                this.xSupplier = xMove;
+                this.ySupplier = yMove;
 
-        this.vaSolver = new VelocityAngleSolver();
-        this.vRpmSolver = new VelocityToRPMSolver(batteryVoltageSupplier, () -> shooter.getMotorRPM());
-        this.fieldFacingAngle = new SwerveRequest.FieldCentricFacingAngle()
-                // Add a 10% deadband
-                .withDeadband(MaxSpeed * 0.1)
-                // Use open-loop control for drive motors
-                .withDriveRequestType(DriveRequestType.OpenLoopVoltage);
+                this.vaSolver = new VelocityAngleSolver();
+                this.vRpmSolver = new VelocityToRPMSolver(batteryVoltageSupplier, () -> shooter.getMotorRPM());
+                this.fieldFacingAngle = new SwerveRequest.FieldCentricFacingAngle()
+                                // Add a 10% deadband
+                                .withDeadband(MaxSpeed * 0.1)
+                                // Use open-loop control for drive motors
+                                .withDriveRequestType(DriveRequestType.OpenLoopVoltage);
 
-        // REQUIRE BOTH: This stops any other drive or shooter commands
-        addRequirements(this.shooter, this.drive);
-    }
-
-    // -----------------------------------------------------------------------------------------------------------------
-    @Override
-    public void execute() {
-        // TODO: Get these values from the controller too.
-        final double shapeScalar = 1;
-        final boolean isBlocked = false;
-
-        /*
-         * 1. One physics calculation to rule them all
-         */
-        // Get required speed data.
-        final ChassisSpeeds robotCentric = drive.getState().Speeds;
-        final Rotation2d heading = drive.getState().Pose.getRotation();
-        final ChassisSpeeds fieldRelative = ChassisSpeeds.fromRobotRelativeSpeeds(robotCentric, heading);
-        final double fieldVx = fieldRelative.vxMetersPerSecond; // x in field frame (blue origin forward)
-        final double fieldVy = fieldRelative.vyMetersPerSecond; // y in field frame
-
-        // Get target data.
-        final Transform3d hubRelativeTransform = aimCamera.getHubRelativeLocation();
-        if (hubRelativeTransform == null) {
-            // TODO: Need a way to handle outside state, or something to handle it.
-            this.cancel();
+                // REQUIRE BOTH: This stops any other drive or shooter commands
+                addRequirements(this.shooter, this.drive);
         }
 
-        // Do some physics.
-        final ShotResult shot = vaSolver.calculate(hubRelativeTransform, heading, fieldVx, fieldVy, shapeScalar,
-                isBlocked);
-        final double motorRPM = vRpmSolver.calculateMotorRPM(shot.getFlyWheelSpeedMPS());
+        // -------------------------------------------------------------------------------------------------------------
+        @Override
+        public void execute() {
+                // TODO: Get these values from the controller too.
+                final double shapeScalar = 1;
+                final boolean isBlocked = false;
 
-        /*
-         * 2. Command the Shooter
-         */
-        shooter.setRPM(motorRPM);
-        shooter.setLaunchAngle(shot.getHoodPitchDegrees());
+                /*
+                 * 1. One physics calculation to rule them all
+                 */
+                // Get required speed data.
+                final ChassisSpeeds robotCentric = drive.getState().Speeds;
+                final Rotation2d heading = drive.getState().Pose.getRotation();
+                final ChassisSpeeds fieldRelative = ChassisSpeeds.fromRobotRelativeSpeeds(robotCentric, heading);
+                final double fieldVx = fieldRelative.vxMetersPerSecond; // x in field frame (blue origin forward)
+                final double fieldVy = fieldRelative.vyMetersPerSecond; // y in field frame
 
-        /*
-         * 3. Command the Drivebase
-         * 
-         * We use the driver's X/Y but OVERRIDE the rotation with our 3D solution.
-         */
-        // Correct any input.
-        final double BACKWARDS_CLOCKWISE = 180;
-        final double BACKWARDS_COUNTER_CLOCKWISE = -180;
-        final double turretYaw = MathUtil.inputModulus(shot.getTurretYawDegrees(), BACKWARDS_COUNTER_CLOCKWISE,
-                BACKWARDS_CLOCKWISE);
-        // Actually drive.
-        drive.setControl(fieldFacingAngle
-                .withVelocityX(xSupplier.getAsDouble())
-                .withVelocityY(ySupplier.getAsDouble())
-                .withTargetDirection(new Rotation2d(turretYaw)));
-    }
+                // Get target data.
+                final Transform3d hubRelativeTransform = aimCamera.getHubRelativeLocation();
+                if (hubRelativeTransform == null) {
+                        // TODO: Need a way to handle outside state, or something to handle it.
+                        this.cancel();
+                }
+
+                // Do some physics.
+                final ShotResult shot = vaSolver.calculate(hubRelativeTransform, heading, fieldVx, fieldVy, shapeScalar,
+                                isBlocked);
+                final double motorRPM = vRpmSolver.calculateMotorRPM(shot.getFlyWheelSpeedMPS());
+
+                /*
+                 * 2. Command the Shooter
+                 */
+                shooter.setRPM(motorRPM);
+
+                /*
+                 * 3. Command the Drivebase
+                 * 
+                 * We use the driver's X/Y but OVERRIDE the rotation with our 3D solution.
+                 */
+                // Correct any input.
+                final double BACKWARDS_CLOCKWISE = 180;
+                final double BACKWARDS_COUNTER_CLOCKWISE = -180;
+                final double turretYaw = MathUtil.inputModulus(shot.getTurretYawDegrees(), BACKWARDS_COUNTER_CLOCKWISE,
+                                BACKWARDS_CLOCKWISE);
+                // Actually drive.
+                drive.setControl(fieldFacingAngle
+                                .withVelocityX(xSupplier.getAsDouble())
+                                .withVelocityY(ySupplier.getAsDouble())
+                                .withTargetDirection(new Rotation2d(turretYaw)));
+        }
 }
