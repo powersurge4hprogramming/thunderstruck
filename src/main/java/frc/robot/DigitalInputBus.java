1bb7e126d416a0f05c308aaac34a7da59d48f0d7
Michael Torres <mrmojorisen0@gmail.com>
2026-02-03 22:54:05 -0500

    Adding some climber stuff.

---
+++ src/main/java/frc/robot/DigitalInputBus.java
@@ -0,0 +1,13 @@
+package frc.robot;
+
+/**
+ * {@summary}
+ * This holds all of the static constant values of where the digital inputs are
+ * plugged into.
+ */
+public class DigitalInputBus {
+    public static final class CLIMBER {
+        public static final byte LEFT_ARM_LIMIT_SWITCH = 0;
+        public static final byte RIGHT_ARM_LIMIT_SWITCH = 0;
+    }
+}
--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -1,9 +1,20 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import frc.robot.DigitalInputBus;
+
 public class Climber extends SubsystemBase {
+    private final DigitalInput leftArmLimit;
+    private final DigitalInput rightARmLimit;
+
+    // -----------------------------------------------------------------------------------------------------------------
+    public Climber() {
+        leftArmLimit = new DigitalInput(DigitalInputBus.CLIMBER.LEFT_ARM_LIMIT_SWITCH);
+        rightARmLimit = new DigitalInput(DigitalInputBus.CLIMBER.RIGHT_ARM_LIMIT_SWITCH);
+    }
 
     /**
      * {@summary}
