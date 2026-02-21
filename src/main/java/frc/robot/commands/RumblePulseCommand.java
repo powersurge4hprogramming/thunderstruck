91aa692beb761b49203321cc384cf3f509565c42
powersurge4hprogramming <58537304+powersurge4hprogramming@users.noreply.github.com>
2026-02-21 14:44:46 -0500

    Initial rumble command for pulsing. Gonna need to do one for dynamic vibration.

---
+++ src/main/java/frc/robot/commands/RumblePulseCommand.java
@@ -0,0 +1,30 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+
+public class RumblePulseCommand extends SequentialCommandGroup {
+    public enum VibrationSide {
+        LEFT,
+        RIGHT,
+        BOTH
+    }
+
+    private final CommandXboxController controller;
+    private final double pulseDurationSeconds;
+    private final double interPulseDurationSeconds;
+    private final double intensity;
+    private final byte numPulses;
+    private final VibrationSide side;
+
+    RumblePulseCommand(final CommandXboxController controller, final double pulseDurationSeconds,
+            final double interPulseDurationSeconds, final double intensity, final byte numPulses,
+            final VibrationSide side) {
+        this.controller = controller;
+        this.pulseDurationSeconds = pulseDurationSeconds;
+        this.interPulseDurationSeconds = interPulseDurationSeconds;
+        this.intensity = intensity;
+        this.numPulses = numPulses;
+        this.side = side;
+    }
+}
