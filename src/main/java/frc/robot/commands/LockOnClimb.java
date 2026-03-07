c2638a92a433e5ab0a92c713103c1dce99d3a664
Michael Torres <mrmojorisen0@gmail.com>
2026-03-06 22:46:21 -0500

    I added a comment so that I remember I have to rotate the robot so that the rear is facing the tower post.

--- src/main/java/frc/robot/commands/LockOnClimb.java
+++ src/main/java/frc/robot/commands/LockOnClimb.java
@@ -35,6 +35,7 @@ public class LockOnClimb extends SequentialCommandGroup {
                         new ApproachLeft(drivetrain, aimCamera, maxSpeed, maxAngularRate),
                         new ApproachRight(drivetrain, aimCamera, maxSpeed, maxAngularRate),
                         () -> approachedFromLeft),
+                // Rotate 180 degrees to face the climber at the post.
                 new Ascend(climber),
                 new WaitCommand(2),
                 new Descend(climber));
