861cd7fd71c4c344ee6f291b5d3ae85568843f8b
powersurge4hprogramming <58537304+powersurge4hprogramming@users.noreply.github.com>
2026-03-07 11:38:40 -0500

    ARE YOU READY TO RUMBLLLLLLLLLLE

--- src/main/java/frc/robot/commands/rumble/RumbleIntensity.java
+++ src/main/java/frc/robot/commands/rumble/RumbleIntensity.java
@@ -5,7 +5,7 @@ public final class RumbleIntensity {
     public static final double LIGHT = 0.2;
     public static final double MEDIUM_LIGHT = 0.4;
     public static final double MEDIUM = 0.5;
-    public static final double HEAVY_LIGHT = 0.8;
+    public static final double MEDIUM_HEAVY = 0.8;
     public static final double HEAVY = 0.9;
     public static final double SUPER_HEAVY = 1.0;
 }
