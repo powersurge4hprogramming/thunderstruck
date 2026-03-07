577f27ce5bbd733cef1d217caed70d9c67e00656
Michael Torres <mrmojorisen0@gmail.com>
2026-03-06 22:06:07 -0500

    Ok, I think that I am done with the FaceWall command. I am not sure I need to implement end() only because my velocities are already zero and if I change the target heading then I am kinda toast.

--- src/main/java/frc/robot/commands/FaceWall.java
+++ src/main/java/frc/robot/commands/FaceWall.java
@@ -6,48 +6,58 @@ import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.swerve.SwerveRequest;
 
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 
 public class FaceWall extends Command {
-    // =================================================================================================================
-    // Private Data Members
-    // =================================================================================================================
-    private final CommandSwerveDrivetrain drivetrain;
-    private final SwerveRequest.FieldCentricFacingAngle fieldFacingAngle;
+        // =============================================================================================================
+        // Private Data Members
+        // =============================================================================================================
+        private final CommandSwerveDrivetrain drivetrain;
+        private final SwerveRequest.FieldCentricFacingAngle fieldFacingAngle;
+        private Rotation2d towerHeading;
 
-    // =================================================================================================================
-    // Package Protected Contructor
-    // =================================================================================================================
-    FaceWall(final double maxSpeed, final double maxAngularRate, final CommandSwerveDrivetrain drivetrain) {
-        this.drivetrain = drivetrain;
-        this.fieldFacingAngle = new SwerveRequest.FieldCentricFacingAngle()
-                // Add a 10% deadband
-                .withDeadband(maxSpeed * 0.1)
-                // Use open-loop control for drive motors
-                .withDriveRequestType(DriveRequestType.OpenLoopVoltage);
+        // =============================================================================================================
+        // Package Protected Contructor
+        // =============================================================================================================
+        FaceWall(final double maxSpeed, final double maxAngularRate, final CommandSwerveDrivetrain drivetrain) {
+                this.towerHeading = null;
+                this.drivetrain = drivetrain;
+                this.fieldFacingAngle = new SwerveRequest.FieldCentricFacingAngle()
+                                // Add a 10% deadband
+                                .withDeadband(maxSpeed * 0.1)
+                                // Use open-loop control for drive motors
+                                .withDriveRequestType(DriveRequestType.OpenLoopVoltage);
 
-        addRequirements(this.drivetrain);
-    }
+                addRequirements(this.drivetrain);
+        }
 
-    // =================================================================================================================
-    // Public Methods
-    // =================================================================================================================
-    @Override
-    public void execute() {
-        // Face the wall.
-        final Optional<Alliance> allianceOptional = DriverStation.getAlliance();
-        final Alliance alliance = allianceOptional
-                .orElseThrow(() -> new RuntimeException("Whaaaaa... No alliance is set?"));
+        // =============================================================================================================
+        // Public Methods
+        // =============================================================================================================
+        @Override
+        public void initialize() {
+                final Optional<Alliance> allianceOptional = DriverStation.getAlliance();
+                final Alliance alliance = allianceOptional
+                                .orElseThrow(() -> new RuntimeException("Whaaaaa... No alliance is set?"));
+                this.towerHeading = (alliance == Alliance.Blue) ? Rotation2d.fromDegrees(180.0)
+                                : Rotation2d.fromDegrees(0.0);
 
-        Rotation2d targetHeading = (alliance == Alliance.Blue) ? Rotation2d.fromDegrees(180.0)
-                : Rotation2d.fromDegrees(0.0);
-        drivetrain.setControl(fieldFacingAngle
-                .withVelocityX(0)
-                .withVelocityY(0)
-                .withTargetDirection(targetHeading));
-    }
+                // Face the wall.
+                drivetrain.setControl(fieldFacingAngle
+                                .withVelocityX(0)
+                                .withVelocityY(0)
+                                .withTargetDirection(towerHeading));
+        }
+
+        // -------------------------------------------------------------------------------------------------------------
+        @Override
+        public boolean isFinished() {
+                final Rotation3d current = drivetrain.getRotation3d();
+                return current.minus(new Rotation3d(towerHeading)).getAngle() == 0;
+        }
 
 }
