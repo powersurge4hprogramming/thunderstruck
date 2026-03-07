edfd19a320e955fdd52169254edb3665c5d44992
Michael Torres <mrmojorisen0@gmail.com>
2026-03-07 00:07:54 -0500

    Added the static class I mentioned in the last commit to hold values for the various rumble intensities. I also made the CANBus class final.

---
+++ src/main/java/frc/robot/commands/rumble/RumbleIntensity.java
@@ -0,0 +1,11 @@
+package frc.robot.commands.rumble;
+
+public final class RumbleIntensity {
+    public static final double VERY_LIGHT = 0.1;
+    public static final double LIGHT = 0.2;
+    public static final double MEDIUM_LIGHT = 0.4;
+    public static final double MEDIUM = 0.5;
+    public static final double HEAVY_LIGHT = 0.8;
+    public static final double HEAVY = 0.9;
+    public static final double SUPER_HEAVY = 1.0;
+}
