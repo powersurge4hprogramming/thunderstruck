4f6f16a04b660407c81d6b5b4232410163e0aae8
Michael Torres <mrmojorisen0@gmail.com>
2026-03-06 23:13:06 -0500

    A tiny optimization. Nice!

--- src/main/java/frc/robot/commands/LockOnClimb.java
+++ src/main/java/frc/robot/commands/LockOnClimb.java
@@ -1,9 +1,7 @@
 package frc.robot.commands;
 
 import edu.wpi.first.math.geometry.Transform3d;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.ConditionalCommand;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.subsystems.Climber;
@@ -12,29 +10,21 @@ import frc.robot.vision.AimCamera;
 
 public class LockOnClimb extends SequentialCommandGroup {
     // =================================================================================================================
-    // Private Data Members
-    // =================================================================================================================
-    private boolean approachedFromLeft;
-
-    // =================================================================================================================
     // Public Methods
     // =================================================================================================================
     public LockOnClimb(final Climber climber, final CommandSwerveDrivetrain drivetrain, final AimCamera aimCamera,
             final double maxSpeed, final double maxAngularRate) {
         addRequirements(climber, drivetrain);
 
-        final Command whatApproachPosition = new InstantCommand(() -> {
-            final Transform3d leftTag = aimCamera.getTowerRelativeLeftLocation();
-            approachedFromLeft = leftTag.getY() > 0;
-        });
-
         addCommands(
-                whatApproachPosition,
                 new FaceWallForward(maxSpeed, maxAngularRate, drivetrain),
                 new ConditionalCommand(
                         new ApproachLeft(drivetrain, aimCamera, maxSpeed, maxAngularRate),
                         new ApproachRight(drivetrain, aimCamera, maxSpeed, maxAngularRate),
-                        () -> approachedFromLeft),
+                        () -> {
+                            final Transform3d leftTag = aimCamera.getTowerRelativeLeftLocation();
+                            return leftTag.getY() > 0;
+                        }),
                 new FaceWallRear(maxSpeed, maxAngularRate, drivetrain),
                 new Ascend(climber),
                 new WaitCommand(2),
