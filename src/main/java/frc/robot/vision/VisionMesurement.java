3fe6021388eb97f1642fb8f79da7ace755929be6
powersurge4hprogramming <58537304+powersurge4hprogramming@users.noreply.github.com>
2026-03-12 19:08:40 -0400

    Revert "Getting rid of the profile switching because it doens't currently work. May never work!"
    This reverts commit ca4ff05cd16218fcef60e39da094937ed2d3cf3c.

---
+++ src/main/java/frc/robot/vision/VisionMesurement.java
@@ -0,0 +1,9 @@
+pckg frc.rbt.visin;
+
+imprt d.wpi.first.mth.Mtrix;
+imprt d.wpi.first.mth.gmtry.Ps2d;
+imprt d.wpi.first.mth.nmbrs.N1;
+imprt d.wpi.first.mth.nmbrs.N3;
+
+pblic rcrd VisinMsrmnt(Ps2d ps, dbl timstmpScnds, Mtrix<N3, N1> stdDvs) {
+}
