818217e047fb7d98a8ec6e0ff11a0f20daf40995
powersurge4hprogramming <58537304+powersurge4hprogramming@users.noreply.github.com>
2026-03-20 14:54:21 -0400

    Adding in the rear camera. Will eat wifi beandwidth--not local wired bandwidth for photonvision--so we should be ok.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.cscore.UsbCamera;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 
@@ -15,6 +17,9 @@ public class Robot extends TimedRobot {
 
     public Robot() {
         robotContainer = new RobotSystem();
+        UsbCamera rearCamera = CameraServer.startAutomaticCapture();
+        rearCamera.setResolution(320, 320);
+        rearCamera.setFPS(15);
     }
 
     @Override
