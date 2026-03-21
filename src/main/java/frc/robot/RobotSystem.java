28363b6d17ccf2f3f65bdca75743deaa736407c7
Michael Torres <mrmojorisen0@gmail.com>
2026-03-21 17:07:43 -0400

    Finally figure out the dumb auto issue. One command was running forever.

--- src/main/java/frc/robot/RobotSystem.java
+++ src/main/java/frc/robot/RobotSystem.java
@@ -184,12 +184,13 @@ public class RobotSystem {
                                 drivetrain // Subsystem ref
                 );
 
-                NamedCommands.registerCommand(EVENT_COLLECT, collector.run(() -> 1));
+                NamedCommands.registerCommand(EVENT_COLLECT, collector.run(() -> 1).withTimeout(4));
                 NamedCommands.registerCommand(EVENT_SHOOT, shooter.manualShootBall(() -> 1)
                                 .alongWith(new WaitCommand(1)
                                                 .andThen(feeder.manualFeederRunIn())
-                                                .withTimeout(4.0)));
-                NamedCommands.registerCommand(EVENT_HOPPER, collector.run(() -> 1));
+                                                .withTimeout(4.0))
+                                .withTimeout(1));
+                NamedCommands.registerCommand(EVENT_HOPPER, collector.run(() -> 1).withTimeout(0.25));
 
                 // Setup the auto UI in Shuffleboard.
                 autoChooser = AutoBuilder.buildAutoChooser();
