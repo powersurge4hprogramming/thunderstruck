474b370fdb27eb1ac9c3eb9b17bdfb6b2234a9c9
powersurge4hprogramming <58537304+powersurge4hprogramming@users.noreply.github.com>
2026-03-22 08:19:09 -0400

    "Oh... oooooohhhhh"

--- src/main/java/frc/robot/RobotSystem.java
+++ src/main/java/frc/robot/RobotSystem.java
@@ -184,13 +184,13 @@ public class RobotSystem {
                                 drivetrain // Subsystem ref
                 );
 
-                NamedCommands.registerCommand(EVENT_COLLECT, collector.run(() -> 1).withTimeout(4));
+                NamedCommands.registerCommand(EVENT_COLLECT, collector.run(() -> -1).withTimeout(4));
                 NamedCommands.registerCommand(EVENT_SHOOT, shooter.manualShootBall(() -> 1)
                                 .alongWith(new WaitCommand(1)
                                                 .andThen(feeder.manualFeederRunIn())
                                                 .withTimeout(4.0))
                                 .withTimeout(7));
-                NamedCommands.registerCommand(EVENT_HOPPER, collector.run(() -> 1).withTimeout(2.5));
+                NamedCommands.registerCommand(EVENT_HOPPER, collector.run(() -> -1).withTimeout(2.5));
 
                 // Setup the auto UI in Shuffleboard.
                 autoChooser = AutoBuilder.buildAutoChooser();
